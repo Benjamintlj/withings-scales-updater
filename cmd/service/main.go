621278284e4b2ec4b57137f0028bcb77696655f6
Ben Lewis-Jones <ben@benlewisjones.com>
2026-08-17 22:46:27 +0100

    feat: get access token

--- cmd/service/main.go
+++ cmd/service/main.go
@@ -7,6 +7,7 @@ import (
 	"log/slog"
 	"net/http"
 	"os"
+	"time"
 
 	"github.com/Benjamintlj/withings-scales-updater/authorization"
 )
@@ -17,12 +18,22 @@ func main() {
 
 	port := flag.String("port", "8080", "port the service will run on")
 	serviceUrl := flag.String("service-url", "https://raspberrypi5.tailbe1abe.ts.net", "the url the service is exposed on")
+	accountWithlingsUrl := flag.String("account-withlings-url", "https://account.withings.com", "withlings account url")
 	flag.Parse()
 
 	clientId := os.Getenv("CLIENT_ID")
+	customerSecret := os.Getenv("CUSTOMER_SECRET")
 
-	au := authorization.New(*serviceUrl, clientId)
+	client := &http.Client{
+		Timeout: 10 * time.Second,
+		CheckRedirect: func(req *http.Request, via []*http.Request) error {
+			return http.ErrUseLastResponse
+		},
+	}
+
+	au := authorization.New(*serviceUrl, clientId, customerSecret, *accountWithlingsUrl, client)
 
 	http.HandleFunc("/login", au.LoginHandler)
+	http.HandleFunc("/get_token", au.TokenHandler)
 	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%v", *port), nil))
 }
